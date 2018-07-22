f993b013a49661a1a4f5ae676ab8465721b8569d
Kartik Chauhan <lkchauhan.725@gmail.com>
2018-07-22 20:24:02 +0530

    working on bsf

--- app/src/main/java/com/dzone/module4/BSF/BSFactivity.java
+++ app/src/main/java/com/dzone/module4/BSF/BSFactivity.java
@@ -1,11 +1,17 @@
 package com.dzone.module4.BSF;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.dzone.module4.R;
 
@@ -45,9 +51,20 @@ public class BSFactivity extends AppCompatActivity {
         award.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri uri = Uri.parse("http://bsf.nic.in/doc/honours/pmg2.pdf"); // missing 'http://' will cause crashed
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+                ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                        connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+                    //we are connected to a network
+
+                    Uri uri = Uri.parse("http://bsf.nic.in/doc/honours/pmg2.pdf"); // missing 'http://' will cause crashed
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+                else
+//                    Toast.makeText(BSFactivity.this,"Check your internet connection !",Toast.LENGTH_SHORT).show();
+                    Snackbar.make(view, "You're not connected to internet. Check your internet connection !", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
             }
         });
     }
